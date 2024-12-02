a73ed177b078d8548333e1cb097680865567368d
MatejFMF <mk54674@student.uni-lj.si>
2024-12-02 10:21:20 +0100

    delal_lean

--- 07-odvisni-tipi/vaje/logic.lean
+++ 07-odvisni-tipi/vaje/logic.lean
@@ -1,7 +1,16 @@
 -- Izomorfizmi
 
 theorem eq1 {A B : Prop} : (A ∧ B) ↔ (B ∧ A) :=
-  sorry
+   by
+    apply Iff.intro
+    . intro h
+      apply And.intro
+      . exact h.right
+      . exact h.left
+    . intro hd
+      apply And.intro
+      . exact hd.right
+      . exact hd.left
 
 theorem eq2 {A B : Prop} : (A ∨ B) ↔ (B ∨ A) :=
   sorry
@@ -16,7 +25,27 @@ theorem eq5 {A B C : Prop} : A ∧ (B ∨ C) ↔ (A ∧ B) ∨ (A ∧ C) :=
   sorry
 
 theorem eq6 {A B C : Prop} : (B ∨ C) → A ↔ (B → A) ∧ (C → A) :=
-  sorry
+  by
+    apply Iff.intro
+    . intro h
+      apply And.intro
+      . intro hb
+        apply h
+        left
+        assumption
+      . intro hc
+        apply h
+        right
+        assumption
+    . intro h hbc
+      cases hbc
+      . apply h.left
+        assumption
+      . apply h.right
+        assumption
+
+
+
 
 theorem eq7 {A B C : Prop} : C → (A ∧ B) ↔ (C → A) ∧ (C → B) :=
   sorry
